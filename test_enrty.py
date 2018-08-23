cd9b451e766e9e4757172dacfe03a0c398275d0d
ryo <@example.com>
2018-08-24 00:50:33 +0900

    change commit timing

--- test_enrty.py
+++ test_enrty.py
@@ -32,6 +32,8 @@ def search_files(file_path_list, pattern):
         # ファイル情報をDBに登録
         regist_file_info(dir_path, file_name)
 
+    db_accessor.commit()
+
     for matched in matched_array:
         if matched:
             # 同一キーワード、同一ファイルの検索結果を削除
@@ -40,6 +42,8 @@ def search_files(file_path_list, pattern):
         # 検索結果登録
         for dat in matched:
             regist_result(pattern, dat['dir_path'], dat['file_name'], dat['line_number'], dat['start'], dat['end'])
+
+    db_accessor.commit()
 
 #------------------------------------------------------------------------------------
 # ファイル情報登録処理
@@ -59,23 +63,16 @@ def regist_file_info(dir_path, file_name):
         # 追加
         db_accessor.insert_file_record(dir_path, file_name, mtime)
 
-    # コミット
-    db_accessor.commit()
-
 #------------------------------------------------------------------------------------
 # 検索結果登録処理
 #------------------------------------------------------------------------------------
 def regist_result(keyword, dir_path, file_name, row_no, start, end):
     # 検索結果登録
     db_accessor.insert_result_record(keyword, dir_path, file_name, row_no, start, end)
-    # コミット
-    db_accessor.commit()
 
 def delete_result(keyword, dir_path, file_name):
     # 検索結果削除
     db_accessor.delete_result_record(keyword, dir_path, file_name)
-    # コミット
-    db_accessor.commit()
 
 #------------------------------------------------------------------------------------
 # 検索関数
